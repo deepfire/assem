a853298ffdfa32f39adb54fd1b9e906a8731e9a2
Samium Gromoff <deepfire@elvees.com>
2008-09-30 21:35:47 +0400

    Take register read protection into account. Return affected list.

--- unturing-mips.lisp
+++ unturing-mips.lisp
@@ -3,48 +3,56 @@
 
 (defun analyse-for-mc24rt-bug (netlist &optional (danger-window 10))
   (declare (optimize (speed 0) (space 0) (debug 3) (safety 3)))
-  (let ((load-list '(:lb :lh :lw :lbu :lhu :lwu :lwl :lwr :ll :l.s :l.d)))
+  (lret ((load-list '(:lb :lh :lw :lbu :lhu :lwu :lwl :lwr :ll :l.s :l.d))
+         hits)
     (labels ((insn-load-p (insn) (member (mnemonics insn) load-list))
              (bb-ins (bb) (unturing:bb-ins bb)) (bb-outs (bb) (unturing:bb-outs bb)))
       (iter (for bb in netlist)
-            (let* ((endangered-regs (make-hash-table)) max-danger)
-              (labels ((collect-endangered-regs (bb allotment)
+            (let* ((endangered-regs (make-hash-table))
+                   (protected-regs (make-hash-table))
+                   max-danger)
+              (labels ((insn-reg-list (role insn params)
+                         ;; (format t "<~S~{ ~S~}>~%" insn (insn-src/dst-spec insn))
+                         (iter (for src/dst-spec in (insn-src/dst-spec insn))
+                               (for param in params)
+                               (when (eq src/dst-spec role)
+                                 (collect param))))
+                       (collect-endangered-regs (bb allotment)
                          (iter (for (nil nil insn . params) in-vector (extent-data bb) with-index i
-                                    from (max 0 (- (extent-length bb) allotment)))
+                                    downto (max 0 (- (extent-length bb) allotment)))
+                               (dolist (prot (insn-reg-list :src insn params))
+                                 (setf (gethash prot protected-regs) t))
                                ;;        <--i-->
                                ;; (------[.....o.....]------)
                                ;; <--dgr-left-->
                                ;; <-----allotment---->
-                               (when (insn-load-p insn)
-                                 ;; that's it -- in all mem->reg load insn formats the victim is 1st insn parameter
-                                 (unless (gethash (first params) endangered-regs) 
-                                   (setf (gethash (first params) endangered-regs)
-                                         (list bb (- allotment (extent-length bb) (- i))))))))
-                       (insn-dstreg-list (insn params)
-                         ;; (format t "<~S~{ ~S~}>~%" insn (insn-src/dst-spec insn))
-                         (and (not (insn-load-p insn))
-                              (iter (for src/dst-spec in (insn-src/dst-spec insn))
-                                    (for param in params)
-                                    (when (eq src/dst-spec :dst)
-                                      (collect param)))))
-                       (note-affected-insns (bb allotment &aux (used-danger (- max-danger allotment)) hits)
+                               (when (and (insn-load-p insn)
+                                          ;; that's it -- in all mem->reg load insn formats the victim is 1st insn parameter
+                                          (not (or (gethash (first params) protected-regs)
+                                                   (gethash (first params) endangered-regs))))
+                                 (setf (gethash (first params) endangered-regs)
+                                       (list bb (- allotment (extent-length bb) (- i)))))))
+                       (note-affected-insns (bb allotment &aux (used-danger (- max-danger allotment)) bb-hits)
                          ;; (format t "eyeing: ~S ~S~%" bb used-danger)
                          (iter (for (nil nil insn . params) in-vector (extent-data bb) with-index i)
                                (for danger-index from used-danger)
-                               (dolist (dstreg (insn-dstreg-list insn params))
-                                 (when-let (vulnspec (gethash dstreg endangered-regs)) ;; vulnerable?
-                                   (destructuring-bind (dmg-bb reg-safety-edge) vulnspec
-                                     ;;        <--i---->
-                                     ;; (------[..rse..d-idx.]----)
-                                     ;; <-reg-dgr->
-                                     ;;        <-----allotment---->
-                                     ;; <-------max-danger-------->
-                                     (when (and (< danger-index reg-safety-edge)
-                                                (not (eq bb dmg-bb)))
-                                       (push (list (+ (extent-base bb) i) dstreg dmg-bb) hits))))))
-                         (when hits
+                               (for dstreg = (first (insn-reg-list :dst insn params))) ;; only care about single destreg
+                               (when-let (vulnspec (and dstreg
+                                                        (not (insn-load-p insn))
+                                                        (gethash dstreg endangered-regs))) ;; vulnerable?
+                                 (destructuring-bind (dmg-bb reg-safety-edge) vulnspec
+                                   ;;        <--i---->
+                                   ;; (------[..rse..d-idx.]----)
+                                   ;; <-reg-dgr->
+                                   ;;        <-----allotment---->
+                                   ;; <-------max-danger-------->
+                                   (when (and (< danger-index reg-safety-edge)
+                                              (not (eq bb dmg-bb)))
+                                     (push (list (+ (extent-base bb) i) dstreg dmg-bb) bb-hits)))))
+                         (when bb-hits
                            (unturing::pprint-object bb t)
-                           (format t "~%affect list:~:{~%addr ~S, reg ~S, damage-bb ~S~}~%~%" hits))))
+                           (format t "~%affect list:~:{~%addr ~S, reg ~S, damage-bb ~S~}~%~%" bb-hits)
+                           (appendf hits bb-hits))))
                 (unturing:mapt-bb-paths #'collect-endangered-regs danger-window bb :key #'bb-ins)
                 (setf max-danger (or (iter (for (nil (nil danger-rest)) in-hashtable endangered-regs)
                                            (maximize danger-rest)) 0))

52567fefee1011ef673fbfee7fd5653d1aff2041
Samium Gromoff <deepfire@elvees.com>
2008-10-03 22:35:12 +0400

    Deliver BB-MC24RT2-VICTIM-P for future woot.

--- unturing-mips.lisp
+++ unturing-mips.lisp
@@ -4,57 +4,66 @@
 
 (defun insn-load-p (insn) (member (mnemonics insn) *load-list*))
 
+(defun bb-ins (bb) (unturing:bb-ins bb))
+
+(defun insn-reg-list (role insn params)
+  (iter (for src/dst-spec in (insn-src/dst-spec insn))
+        (for param in params)
+        (when (eq src/dst-spec role)
+          (collect param))))
+
+(defun update-protected/endangered-sets (bb allotment protected endangered)
+  "Update PROTECTED/ENDANGERED hash sets in an inwards-reverse walk from BB, with ALLOTMENT edge length left."
+  (iter (for (nil nil insn . params) in-vector (extent-data bb) with-index i
+             downto (max 0 (- (extent-length bb) allotment)))
+        (dolist (prot (insn-reg-list :src insn params))
+          (setf (gethash prot protected) t))
+        ;;        <--i-->
+        ;; (------[.....o.....]------)
+        ;; <--dgr-left-->
+        ;; <-----allotment---->
+        (when (and (insn-load-p insn)
+                   ;; in all mem->reg insn formats destreg is first parameter
+                   (not (or (gethash (first params) protected)
+                            (gethash (first params) endangered))))
+          (setf (gethash (first params) endangered)
+                (list bb (- allotment (extent-length bb) (- i)) (+ (extent-base bb) i))))))
+
+(defun maximum-set-danger (set)
+  "Find peak danger in SET."
+  (or (iter (for (nil (nil danger-rest)) in-hashtable set)
+            (maximize danger-rest)) 0))
+
+(defun bb-endangered-p (bb max-danger protected endangered)
+  ;; (format t "eyeing: ~S ~S~%" bb used-danger)
+  (iter (for (nil nil insn . params) in-vector (extent-data bb))
+        (for i below max-danger)
+        (iter (for srcreg in (insn-reg-list :src insn params))
+              (setf (gethash srcreg protected) t))
+        (for dstreg = (first (insn-reg-list :dst insn params)))
+        (when-let (vulnspec (and dstreg (not (insn-load-p insn))
+                                 (not (gethash dstreg protected))
+                                 (gethash dstreg endangered))) ;; vulnerable?
+          (destructuring-bind (dmg-bb reg-safety-edge aggr-addr) vulnspec
+            ;;        <--i---->
+            ;; (------[..rse..d-idx.]----)
+            ;; <-reg-dgr->
+            ;;        <-----allotment---->
+            ;; <-------max-danger-------->
+            (when (and (< i reg-safety-edge)
+                       (not (eq bb dmg-bb)))
+              (return (values bb (+ (extent-base bb) i) dstreg dmg-bb aggr-addr)))))))
+
 (defun bb-mc24rt2-victim-p (node &optional (danger-window 10))
   (declare (optimize (speed 0) (space 0) (debug 3) (safety 3)))
   (let* ((endangered-regs (make-hash-table))
          (protected-regs (make-hash-table)))
-    (labels ((bb-ins (bb) (unturing:bb-ins bb))
-             (insn-reg-list (role insn params)
-               ;; (format t "<~S~{ ~S~}>~%" insn (insn-src/dst-spec insn))
-               (iter (for src/dst-spec in (insn-src/dst-spec insn))
-                     (for param in params)
-                     (when (eq src/dst-spec role)
-                       (collect param))))
-             (collect-endangered-regs (bb allotment)
-               (iter (for (nil nil insn . params) in-vector (extent-data bb) with-index i
-                          downto (max 0 (- (extent-length bb) allotment)))
-                     (dolist (prot (insn-reg-list :src insn params))
-                       (setf (gethash prot protected-regs) t))
-                     ;;        <--i-->
-                     ;; (------[.....o.....]------)
-                     ;; <--dgr-left-->
-                     ;; <-----allotment---->
-                     (when (and (insn-load-p insn)
-                                ;; in all mem->reg insn formats destreg is first parameter
-                                (not (or (gethash (first params) protected-regs)
-                                         (gethash (first params) endangered-regs))))
-                       (setf (gethash (first params) endangered-regs)
-                             (list bb (- allotment (extent-length bb) (- i)) (+ (extent-base bb) i))))))
-             (bb-affected-p (bb danger-window)
-               ;; (format t "eyeing: ~S ~S~%" bb used-danger)
-               (iter (for (nil nil insn . params) in-vector (extent-data bb))
-                     (for i below danger-window)
-                     (iter (for srcreg in (insn-reg-list :src insn params))
-                           (setf (gethash srcreg protected-regs) t))
-                     (for dstreg = (first (insn-reg-list :dst insn params)))
-                     (when-let (vulnspec (and dstreg (not (insn-load-p insn))
-                                              (not (gethash dstreg protected-regs))
-                                              (gethash dstreg endangered-regs))) ;; vulnerable?
-                       (destructuring-bind (dmg-bb reg-safety-edge aggr-addr) vulnspec
-                         ;;        <--i---->
-                         ;; (------[..rse..d-idx.]----)
-                         ;; <-reg-dgr->
-                         ;;        <-----allotment---->
-                         ;; <-------max-danger-------->
-                         (when (and (< i reg-safety-edge)
-                                    (not (eq bb dmg-bb)))
-                           (return (values bb (+ (extent-base bb) i) dstreg dmg-bb aggr-addr))))))))
-      (dolist (entrant (bb-ins node))
-        (unturing:mapt-bb-paths #'collect-endangered-regs danger-window entrant :key #'bb-ins))
-      (let ((max-danger (or (iter (for (nil (nil danger-rest)) in-hashtable endangered-regs)
-                                  (maximize danger-rest)) 0)))
-        (when (plusp max-danger)
-          (bb-affected-p node max-danger))))))
+    (dolist (entrant (bb-ins node))
+      (unturing:mapt-bb-paths (rcurry #'update-protected/endangered-sets protected-regs endangered-regs)
+                              danger-window entrant :key #'bb-ins))
+    (let ((max-danger (maximum-set-danger endangered-regs)))
+      (when (plusp max-danger)
+        (bb-endangered-p node max-danger protected-regs endangered-regs)))))
 
 (defun find-mc24rt2-victims (bbnet &optional (danger-window 10))
   (iter (for bb in bbnet)
